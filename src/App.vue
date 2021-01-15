cda4ab2f39d778110846f174f1e158a95381e6ed
sukuna <nitipon.apaisri@gmail.com>
2021-01-15 15:07:02 +0100

    added cursor

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,35 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-  @import url('https://fonts.googleapis.com/css2?family=Imbue:wght@500&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap');
-  
-  h1{
-    font-family: 'Imbue', serif;
-    letter-spacing: 0.5rem;
-    font-size: 3rem;
-    text-transform: uppercase;
-  }
-  p, button{
-    font-family: 'Raleway', sans-serif;
-    font-size: 1.6rem;
-  }
-  button{
-    outline: none;
-    background: transparent;
-    border: 0.2rem solid black;
-    border-radius: 0.6rem;
-    padding: 1rem;
-    &:hover{
-      background-color: pink;
-    }
-    &:active{
-      background-color: black;
-      color: white;
-    }
-  }
+@import url("https://fonts.googleapis.com/css2?family=Imbue:wght@500&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap");
 
+h1 {
+  font-family: "Imbue", serif;
+  letter-spacing: 0.5rem;
+  font-size: 3rem;
+  text-transform: uppercase;
+}
+p,
+button {
+  font-family: "Raleway", sans-serif;
+  font-size: 1.6rem;
+}
+button {
+  outline: none;
+  background: transparent;
+  border: 0.2rem solid black;
+  border-radius: 0.6rem;
+  padding: 1rem;
+  cursor: pointer;
+  &:hover {
+    background-color: pink;
+  }
+  &:active {
+    background-color: black;
+    color: white;
+  }
+}
 </style>
