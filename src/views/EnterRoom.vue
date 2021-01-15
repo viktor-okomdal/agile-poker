ac236fe74e66fd286fe3de90ff8eec5f0b693966
sukuna <nitipon.apaisri@gmail.com>
2021-01-15 14:28:03 +0100

    added view for enter room

---
+++ src/views/EnterRoom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <EnterRoom />
+  </div>
+</template>
+
+<script>
+import EnterRoom from "@/components/EnterRoomComp.vue";
+export default {
+  components: {
+    EnterRoom,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,13 @@
 <template>
   <div class="home">
     <Header class="header" />
-    <EnterRoom />
   </div>
 </template>
 
 <script>
-import EnterRoom from "@/components/EnterRoom.vue";
 import Header from "@/components/Header.vue";
 export default {
   components: {
-    EnterRoom,
     Header,
   },
 };
